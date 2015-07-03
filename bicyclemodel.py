22b89f4ccfa6afa4de68fdd1a179352959beca2a
squartle <saquartey@gmail.com>
2015-07-02 22:33:59 -0700

    thursday

---
+++ bicyclemodel.py
@@ -0,0 +1,51 @@
+
+class Bicycle(object):
+    def __init__(self, model, weight, prod_cost):
+        self.model = model
+        self.weight = weight
+        self.prod_cost = prod_cost
+
+class Shop(object):
+    def __init__(self, name, margin, inventory):
+        self.name = name
+        self.inventory = inventory
+        self.margin = margin
+        self.profit = profit
+
+    def price(self, margin, Bicycle):
+        price = (margin + 1) * prod_cost
+        print price
+
+    def profit(self, margin, Bicycle):
+        profit = price - prod_cost
+        print profit
+
+class Customer(object):
+    def __init__(self, firstname, funds,):
+        self.firstname = firstname
+        self.funds = funds
+        self.purchase = purchase
+        self.own = customer_inventory
+
+    def own(self):
+        customer_inventory = ()
+
+    def purchase(self, funds):
+        if funds >= price:
+            customer_inventory.append(model)
+        else:
+            print "I can't afford that!"
+
+speedRacer = Bicycle("racing", 12, 220)
+beachBum = Bicycle("casual", 34, 159)
+urbanCyclist = Bycicle("hybrid", 6, 583)
+cruiserClassy = Bicycle("casual", 12, 694)
+ultimateArmstrong = Bicycle("racing", 8, 743)
+happyHipster = Bicycle("hybrid", 15, 325)
+
+OffRampBikes = Shop("Off Ramp Bicycles", .20, ORBinventory)
+pMultiplier = margin + 1
+ORBinventory = []
+
+def initInventory():
+    for 
